eea87d9eb1c6d445dd48bb99d1d98a41ddb3a49c
Murphy McQuet <murphymcquet@Murphys-MacBook-Air.local>
2023-07-17 21:48:13 -0700

    Fist commit

---
+++ scrumDinger/scrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumDingerApp.swift
+//  scrumDinger
+//
+//  Created by Murphy McQuet on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct scrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
